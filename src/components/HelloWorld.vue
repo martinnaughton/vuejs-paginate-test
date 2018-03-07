d3a331222a63cd3a5e86759551a2dbb509dde6e8
martinnaughton <martin.oneachtain@gmail.com>
2018-03-07 17:10:24 +0000

    Added the paginate plugin to the project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <paginate
+  :page-count="20"
+  :click-handler="functionName"
+  :prev-text="'Prev'"
+  :next-text="'Next'"
+  :container-class="'pagination'"
+  :page-class="'page-item'">
+</paginate>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.pagination {
+  margin: 5% 25%;
+}
+
+.page-item.active {
+  border-radius: 0px;
+  position: static !important;
+  background-color: #ed6528 !important;
+}
+
+</style>
